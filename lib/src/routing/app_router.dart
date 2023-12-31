c9414bdf57ac8077ff248717150327da359628f5
Sumith Damodaran <sumithpd@gmail.com>
2023-12-31 01:02:46 +0100

    Update iOS app icons and modify authentication screens

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,9 @@
+import 'dart:js';
+
+import 'package:faker_app_flutter_firebase/src/routing/go_router_refresh_stream.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,6 +16,21 @@ final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedin = FirebaseAuth.instance.currentUser != null;
+      if (isLoggedin) {
+        if (state.uri.path == '/sign-in') {
+          return '/profile';
+        }
+      } else {
+        if (state.uri.path == '/profile') {
+          return '/sign-in';
+        }
+      }
+      return null;
+    },
+    refreshListenable:
+        GoRouterRefreshStream(FirebaseAuth.instance.authStateChanges()),
     routes: [
       GoRoute(
         path: '/sign-in',
