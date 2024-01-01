91abd630dde31f83af37156772a18035a85d2092
Sumith Damodaran <sumithpd@gmail.com>
2024-01-01 12:21:42 +0100

    Add job creation and deletion functionality

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker_app_flutter_firebase/src/data/job.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,12 +13,33 @@ class FirestoreRepository {
       'uid': uid,
       'title': title,
       'company': company,
+      'createdAt': FieldValue.serverTimestamp(),
     });
     debugPrint('Added document: ${docRef.id}');
   }
 
-  Query<Map<String, dynamic>> jobsQuery() {
-    return _firestore.collection('jobs');
+  Future<void> updateJob(
+          String jobId, String uid, String title, String company) =>
+      _firestore.collection('jobs').doc(jobId).update({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+
+  Query<Job> jobsQuery(
+    String uid,
+  ) {
+    return _firestore
+        .collection('jobs')
+        .withConverter(
+            fromFirestore: (snapshot, _) => Job.fromMap(snapshot.data()!),
+            toFirestore: (job, _) => job.toMap())
+        // .where('uid', isEqualTo: uid)
+        .orderBy('createdAt', descending: true);
+  }
+
+  void deleteJob(String uid, String jobId) {
+    _firestore.collection('jobs').doc(jobId).delete();
   }
 }
 
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' hide Job;
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../data/firestore_repository.dart';
+import '../data/job.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -44,14 +45,38 @@ class JobsListView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    final user = ref.read(firebaseAuthProvider).currentUser;
+    return FirestoreListView<Job>(
+      query: firestoreRepository.jobsQuery(user!.uid),
+      pageSize: 20,
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: Container(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            ref.read(firestoreRepositoryProvider).deleteJob(user.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            trailing: job.createdAt != null
+                ? Text(job.createdAt!.toLocal().toString(),
+                    style: Theme.of(context).textTheme.bodySmall)
+                : null,
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
 
-    return FirestoreListView(
-      query: firestoreRepository.jobsQuery(),
-      itemBuilder: (BuildContext context,
-          QueryDocumentSnapshot<Map<String, dynamic>> doc) {
-        return ListTile(
-          title: Text(doc['title']),
-          subtitle: Text(doc['company']),
+              ref.read(firestoreRepositoryProvider).updateJob(
+                    doc.id,
+                    user!.uid,
+                    faker.job.title(),
+                    faker.company.name(),
+                  );
+            },
+          ),
         );
       },
     );
